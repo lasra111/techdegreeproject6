adaa54cde427d61a963b09ed2a78a8fa5790c329
lasra111 <90154983+lasra111@users.noreply.github.com>
2022-03-31 15:10:31 -0600

    changes

---
+++ game_show_app_v1.2/app.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+
+let qwerty = document.getElementById('qwerty');
+let phrase = document.getElementById('phrase');
+let button = document.getElementsByClassName('btn__reset');
+let missed = 0;
+let start = document.getElementsByClassName('start');
+let overlay = document.getElementById('overlay')
+let phrases = [
+  'veni vidi vici',
+  'what is the worst that can happen',
+  'violence never settles anything',
+  'there is nothing impossible to those who will try',
+  'thank god it is almost the weekend'
+];
+
+overlay.addEventListener('click', (e)=> {
+  if(e.target.className==='btn__reset'){
+for(i=0; i<start.length; i++){
+  start[i].style.display='none';
+}
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
